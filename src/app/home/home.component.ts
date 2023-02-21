b09b87097b90b539787d76054fef49279a1941ee
Zaki <qaisharzaki@yahoo.com>
2023-02-21 12:40:21 +0530

    fd

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,4 +6,5 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+ 
 }
